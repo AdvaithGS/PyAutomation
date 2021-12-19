77746763ddea38ebd06b6927a33b44b639bb8084
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-19 14:36:30 +0530

    changed startup.py

--- startup.py
+++ startup.py
@@ -5,9 +5,13 @@ from pyautogui import hotkey
 time = int(strftime('%H'))
 popen('C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')
 if time in range(8,10):
+    open_new_tab('https://discord.com/app')
+    open_new_tab('https://mail.google.com')
     open_new_tab('https://teams.microsoft.com')
 elif time in range(9,12):
-    hotkey('Win','6')
+    hotkey('Win','6')#opens whatsapp on my pc
+    open_new_tab('https://discord.com/app')
+    open_new_tab('https://mail.google.com')
     popen('C:\\Users\\K Padmavathy\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe')
 else:
     popen('C:\Windows\explorer.exe')
