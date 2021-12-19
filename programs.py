6e2d22f91b71f99aa5350d8b8c56e8ced63380d3
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-19 13:11:19 +0530

    added sql and done some more work and handling

--- programs.py
+++ programs.py
@@ -3,7 +3,7 @@ from os import popen
 from webbrowser import open_new_tab
 db = SqliteDict('./db.sqlite',autocommit = True)
 while True:
-    prog = input('Enter program/app to load: ')
+    prog = input('Enter command ')
     try:
         if db[prog][0] == 1:
             popen(db[prog][1])            
@@ -12,5 +12,9 @@ while True:
         print(f'Opening {prog} at {db[prog][1]}')
     except:
         if prog.startswith('add'):
-            
+            lst = list(prog.split(' ',3))
+            name,num,loc = lst[1],int(lst[2]),lst[3]
+            db[name] = [num,loc] 
+        elif prog.startswith('exit'):
+            exit()  
         print('Not available')
