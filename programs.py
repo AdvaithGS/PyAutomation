5f02889b4c6e937bd9aa2a6dee0e881a25dfd20b
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-20 16:17:25 +0530

    made corrections and added hotkeys

--- programs.py
+++ programs.py
@@ -1,14 +1,20 @@
 from sqlitedict import SqliteDict
 from os import popen
 from webbrowser import open_new_tab
-db = SqliteDict('./db.sqlite',autocommit = True)
+from pyautogui import hotkey
+db = SqliteDict('./db.sqlite', autocommit = True)
 while True:
-    prog = input('Enter command: ')
+    prog = input('Enter command >> ')
     try:
         if db[prog][0] == 'app':
             popen(db[prog][1])            
-        else:
+        elif db[prog][0] == 'url':
             open_new_tab(db[prog][1])
+        elif db[prog][0] == 'hotkey':
+            try:
+                hotkey(db[prog][1].split('+')[0],db[prog][1].split('+')[1],db[prog][1].split('+')[2])
+            except:
+                hotkey(db[prog][1].split('+')[0],db[prog][1].split('+')[1])
         print(f'Opening {prog} at {db[prog][1]}')
     except:
         if prog.startswith('add'):
@@ -23,12 +29,12 @@ while True:
             exit() 
         elif prog.startswith('remove'):
             com,name = prog.split()
-            if input(f'Are you sure you want to remove {db[name]}? : ').lower() == 'y':
+            if input(f'Are you sure you want to remove {name} at {db[name][1]}? : ').lower() == 'y':
                  del db[name]
                  print('Deleted.')
         elif prog.startswith('list'):
             i = 1
-            for key in db.keys():
+            for key in list(db.keys()):
                 print(f'   {i}. {key} - {db[key][1]}',end = '\n')
                 i += 1
         else:
