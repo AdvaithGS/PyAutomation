d2104bd9afa72d0f0a3afdc17beb262d770a1a07
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-03-01 00:35:12 +0530

    edited popen

--- startup.py
+++ startup.py
@@ -5,18 +5,18 @@ choice= int(input('Enter choice: '))
 if choice == 1:
     open_new_tab('https://discord.com/app')
     open_new_tab('https://mail.google.com')
-    popen('C:\\Users\\K Padmavathy\\AppData\\Local\\Microsoft\\Teams\\update.exe')
+    x = popen('C:\\Users\\K Padmavathy\\AppData\\Local\\Microsoft\\Teams\\update.exe')
     open_new_tab('https://top.gg/bot/792458754208956466/vote')
     open_new_tab('https://discordbotlist.com/bots/astrobot-2515/upvote')
 elif choice == 2:
     hotkey('Win','5')#opens whatsapp on my pc
     open_new_tab('https://discord.com/app')
     open_new_tab('https://mail.google.com')
-    popen('C:\\Users\\K Padmavathy\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe')
+    x = popen('C:\\Users\\K Padmavathy\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe')
     open_new_tab('https://top.gg/bot/792458754208956466/vote')
     open_new_tab('https://discordbotlist.com/bots/astrobot-2515/upvote')
-else:
-    popen('C:\Windows\explorer.exe')
+elif choice == 3:
+    x = popen('C:\Windows\explorer.exe')
     open_new_tab('https://discord.com/app')
     open_new_tab('https://mail.google.com')
     open_new_tab('https://top.gg/bot/792458754208956466/vote')
