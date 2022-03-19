558d9d3caf2d4fa8d80f1d2c075a0c5bd615852a
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2022-03-19 17:28:38 +0530

    new database saver

--- contacts.py
+++ contacts.py
@@ -1,6 +1,7 @@
 from csv import reader
 from sqlite3 import connect
 from pyperclip import copy
+from time import sleep
 conn = connect('db.sql')
 c = conn.cursor()
 c.execute('select * from contacts')
@@ -12,6 +13,7 @@ l= []
 with open(input('Enter file name: '), 'r') as f:
   x = list(reader(f))
   print(f'Found {len(x)} contacts')
+  sleep(1.15)
   for i in range(len(x[1:])-1):
     if  x[1:][i+1][0] == '' or x[1:][i+1][0] == ' ':
       continue
@@ -31,5 +33,11 @@ with open(input('Enter file name: '), 'r') as f:
       print(f'The number is now {number}')
     l.append([name,number])
 print()
-print('Here are the changed numbers')
-    
+sleep(1.15)
+print('Here are the changed numbers:')
+for i,j in l:
+  print(i,j)
+  if (i,j) not in contacts:
+    print('Saving new contact to db')
+    with conn:
+      c.execute(f'insert into contacts(name,number) values ("{i}","{j}")')
